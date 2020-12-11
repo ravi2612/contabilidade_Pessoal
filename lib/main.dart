52b63abd79b8c04b9b468b640b5112851de8d71b
ravi2612 <ravinavarro26@gmail.com>
2020-12-10 22:48:11 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,58 @@
+import 'package:contabilidade_Pessoal/transaction.dart';
 import 'package:flutter/material.dart';
-
 
 main() => (ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-      
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Corrida',
+      value: 300,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.cen,
-        children:<Widget> [
-          Container(
-            width: double.infinity,
-            alignment: Alignment.center,
-            
-            child: Card(
-              color: Colors.blueAccent,
-              child: Text('Grafico'),
-              elevation: 5,
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Card(
+                color: Colors.blueAccent,
+                child: Text('Grafico'),
+                elevation: 5,
+              ),
             ),
-          ),
-          Card(
-            child: Text('Lista de Transaçoes'),
-          )
-        ],
-      )
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Text(tr.title),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
